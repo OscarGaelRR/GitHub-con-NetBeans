a38b3fc08b81c3bcf0825e6d520e30587d11d4b2
Oscar Ramírez <cuyosbebes3@gmail.com>
2024-11-23 12:43:13 -0600

    Agregué el control de peso XD

--- src/main/java/com/mycompany/githubconnetbeans/GitHubConNetBeans.java
+++ src/main/java/com/mycompany/githubconnetbeans/GitHubConNetBeans.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.githubconnetbeans;
 
+import java.util.Scanner;
 /**
  *
  * @author Warrior HP
@@ -12,5 +13,15 @@ public class GitHubConNetBeans {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        float peso;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu peso en kg: ");
+        peso = scanner.nextFloat();
+        if(peso > 90.0){
+            System.out.println("OYE GENDA, ECUCHATE ETO TU TU TU TUTU BABABA BABA ");
+        }
+        else{
+            System.out.println("Normal xd");
+        }
     }
 }
